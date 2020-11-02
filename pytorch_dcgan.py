8bf28aa5c384e2d76c792c595a0216c2b713a811
imgac00 <72339221+imgac00@users.noreply.github.com>
2020-11-02 13:33:11 +0800

    Update pytorch_dcgan.py

--- pytorch_dcgan.py
+++ pytorch_dcgan.py
@@ -117,6 +117,15 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
+            """”in_channels(int) – 输入信号的通道数---nz
+                out_channels(int) – 卷积产生的通道数---ngf
+                kerner_size(int or tuple) - 卷积核的大小---4
+                stride(int or tuple,optional) - 卷积步长---1
+                padding(int or tuple, optional) - 输入的每一条边补充0的层数---0
+                output_padding(int or tuple, optional) - 输出的每一条边补充0的层数
+                dilation(int or tuple, optional) – 卷积核元素之间的间距
+                groups(int, optional) – 从输入通道到输出通道的阻塞连接数
+                bias(bool, optional) - 如果bias=True，添加偏置"""
             # input is Z, going into a convolution卷积 stride步 padding
             nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False), #偏向
             nn.BatchNorm2d(ngf * 8),
